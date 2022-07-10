b98171c02ee8f13f6d51e38a64d5a63a2a6a75cc
Samhith89492 <106803372+Samhith89492@users.noreply.github.com>
2022-07-10 16:03:22 +0530

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,5 +1,6 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Universal Hub (Beta)", "Sentinel")
+
 local Tab = Window:NewTab("Main")
 
 local MainSection = Tab:NewSection("Main")
@@ -66,4 +67,95 @@ end)
 
 MainSection:NewSlider("Gravity", "Changes your Gravity", 196, 0, function(v)
     game.Workspace.Gravity = v
+end)
+
+local ESPTab = Window:NewTab("ESP")
+local ESPSection = ESPTab:NewSection("ESP")
+
+ESPSection:NewButton("Chams", "Enables chams", function()
+    _G.FriendColor = Color3.fromRGB(0, 0, 255)
+_G.EnemyColor = Color3.fromRGB(255, 0, 0)
+_G.UseTeamColor = false
+
+--------------------------------------------------------------------
+
+if _G.Reantheajfdfjdgse then
+    return
+end
+
+_G.Reantheajfdfjdgse = "susan"
+
+local coregui = game:GetService("CoreGui")
+local players = game:GetService("Players")
+local plr = players.LocalPlayer
+
+local highlights = {}
+
+function esp(target, color)
+    pcall(function()
+        if target.Character then
+            if not highlights[target] then
+                local highlight = Instance.new("Highlight", coregui)
+                highlight.Name = target.Name
+                highlight.Adornee = target.Character
+                highlight.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
+                highlight.FillColor = color
+                highlights[target] = highlight
+            else
+                highlights[target].FillColor = color
+            end
+        end
+    end)
+end
+
+players.PlayerAdded:Connect(function(v)
+    v.CharacterAdded:Connect(function()
+        esp(v, _G.UseTeamColor and v.TeamColor.Color or ((plr.TeamColor == v.TeamColor) and _G.FriendColor or _G.EnemyColor))
+    end)
+end)
+
+players.PlayerRemoving:Connect(function(v)
+    if highlights[v] then
+        highlights[v]:Destroy()
+        highlights[v] = nil
+    end
+end)
+
+for i, v in pairs(players:GetPlayers()) do
+    if v ~= plr then
+        local color = _G.UseTeamColor and v.TeamColor.Color or ((plr.TeamColor == v.TeamColor) and _G.FriendColor or _G.EnemyColor)
+        v.CharacterAdded:Connect(function()
+            local color = _G.UseTeamColor and v.TeamColor.Color or ((plr.TeamColor == v.TeamColor) and _G.FriendColor or _G.EnemyColor)
+            esp(v, color)
+        end)
+        
+        esp(v, color)
+    end
+end
+
+while task.wait() do
+    for i, v in pairs(highlights) do
+        local color = _G.UseTeamColor and i.TeamColor.Color or ((plr.TeamColor == i.TeamColor) and _G.FriendColor or _G.EnemyColor)
+        v.FillColor = color
+    end
+end
+end)
+
+local CreditsTab = Window:NewTab("Credits")
+
+local CreditsSection = CreditsTab:NewSection("Credits")
+
+CreditsSection:NewButton("SamhithWasTaken#1874 aka myself lol", "credits to my self lol", function()
+    print(ok)
+end)
+
+CreditsSection:NewButton("Randomguy#6407", "credits to him for chams", function()
+    print(ok)
+end)
+
+
+MiscSection:NewButton("Infinite Jump", "Allows you to jump in the air", function()
+    game:GetService("UserInputService").JumpRequest:connect(function()
+            game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
+    end)
 end)
