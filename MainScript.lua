f81545005ea3a42cf241faed0db5276ea39bb3e6
Samhith89492 <106803372+Samhith89492@users.noreply.github.com>
2022-07-11 13:22:55 +0530

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Universal Hub (Beta)", "Sentinel")
+local Window = Library.CreateLib("Universal Hub (Beta)", "BloodTheme")
 
 local Tab = Window:NewTab("Main")
 
